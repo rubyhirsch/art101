8c4c9c456663397a0ba685897e15c8a56891c538
rubyhirsch <71003519+rubyhirsch@users.noreply.github.com>
2024-04-23 19:07:52 -0700

    Lab 5: Data Types and Variables

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Data Types and Variables
+// Author: Ruby Hirsch, Franchesca Boongaling
+// Date: 4/23/24
 
-// Constants
+// Variables
+const make = "Honda";
+const model = "CRV";
+const color = "Silver";
+const year = 2014;
 
-// Functions
+var currentYear = 2024;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// Calculate
+var age = currentYear - year;
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Output
+document.writeln("Make: " + make + '<br>');
+document.writeln("Model: " + model + '<br>');
+document.writeln("Color: " + color + '<br>');
+document.writeln("Year: " + year + '<br>');
